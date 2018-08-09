8bc702e8c5463b889290e0a61bcfe29f6df443d2
= <stevekitkat@gmail.com>
2018-08-09 22:07:32 +0700

    Issue with parcel

--- domain/src/main/java/me/androidbox/domain/models/BookDetails.kt
+++ domain/src/main/java/me/androidbox/domain/models/BookDetails.kt
@@ -3,8 +3,8 @@ package me.androidbox.domain.models
 import org.parceler.Parcel
 import org.parceler.ParcelConstructor
 
-//@Parcel(Parcel.Serialization.BEAN)
-class BookDetails(
+@Parcel(Parcel.Serialization.BEAN)
+class BookDetails @ParcelConstructor constructor(
         val title: String,
         val description: String,
         val author: String,
